5735770da6234648140665d184ea797b30f106d4
Peter Bodnar <ptbdnr@google.com>
2024-10-12 00:52:17 +0100

    feat: semantic kernel

---
+++ llm_sdk/azure_semantic_kernel/sdk/python/src/prompt_template.py
@@ -0,0 +1,69 @@
+import os
+import asyncio
+
+import dotenv
+
+from semantic_kernel import Kernel
+from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
+from semantic_kernel.prompt_template import InputVariable, PromptTemplateConfig
+
+dotenv.load_dotenv()
+
+
+kernel = Kernel()  # Initialize the kernel
+service_id = 'chat-gpt'
+llm = AzureChatCompletion(
+    service_id=service_id,
+    # base_url=os.environ["AZURE_OPENAI_ENDPOINT"],  # bug
+    api_key=os.environ["AZURE_OPENAI_API_KEY"],
+    deployment_name=os.environ["AZURE_OPENAI_CHAT_DEPLOYMENT_NAME"],
+    api_version=os.environ["AZURE_OPENAI_API_VERSION"],
+)
+kernel.add_service(llm)
+req_settings = kernel.get_prompt_execution_settings_from_service_id(service_id)
+
+# Create a prompt template
+prompt_template = """Answer the question in {{$style}}. {{$text}}"""
+
+# Compile the prompt template
+prompt_template_config = PromptTemplateConfig(
+    template=prompt_template,
+    name="asnwer_question_template",
+    template_format="semantic-kernel",
+    input_variables=[
+        InputVariable(
+            name="style",
+            description="The requested style",
+            is_required=True
+        ),
+        InputVariable(
+            name="text",
+            description="The user input text",
+            is_required=True
+        ),
+    ],
+    execution_settings=req_settings
+)
+aswer_question = kernel.add_function(
+    function_name="answer_question_function",
+    plugin_name="answer_question_plugin",
+    prompt_template_config=prompt_template_config,
+)
+
+
+async def main(kernel: Kernel):
+
+    # Invoke the LLM
+    result = await kernel.invoke(
+        function=aswer_question,
+        style="technical details",
+        text="who are you?"
+    )
+
+    print('=' * 16 + '\n' + 'LLM OUTPUT')
+    print(str(result))
+
+
+# Run the main function
+if __name__ == "__main__":
+    asyncio.run(main(kernel))
--- llm_sdk/azure_semantic_kernel/sdk/python/src/q_and_a.py
+++ llm_sdk/azure_semantic_kernel/sdk/python/src/q_and_a.py
@@ -4,7 +4,9 @@ import asyncio
 import dotenv
 
 from semantic_kernel import Kernel
-from semantic_kernel.connectors.ai.prompt_execution_settings import PromptExecutionSettings
+from semantic_kernel.connectors.ai.prompt_execution_settings import (
+    PromptExecutionSettings
+)
 from semantic_kernel.contents.chat_history import ChatHistory
 from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
 
@@ -13,14 +15,14 @@ dotenv.load_dotenv()
 
 kernel = Kernel()  # Initialize the kernel
 service_id = 'chat-gpt'
-model = AzureChatCompletion(
+llm = AzureChatCompletion(
     service_id=service_id,
     # base_url=os.environ["AZURE_OPENAI_ENDPOINT"],  # bug
     api_key=os.environ["AZURE_OPENAI_API_KEY"],
     deployment_name=os.environ["AZURE_OPENAI_CHAT_DEPLOYMENT_NAME"],
     api_version=os.environ["AZURE_OPENAI_API_VERSION"],
 )
-kernel.add_service(model)
+kernel.add_service(llm)
 req_settings = kernel.get_prompt_execution_settings_from_service_id(service_id)
 
 
@@ -29,14 +31,14 @@ async def main(kernel: Kernel, req_settings: PromptExecutionSettings):
     messages.add_system_message("Answer the question in technical details.")
     messages.add_user_message("who are you?")
 
-    # Invoke the model
-    result = await model.get_chat_message_content(
+    # Invoke the LLM
+    result = await llm.get_chat_message_content(
         chat_history=messages,
         kernel=kernel,
         settings=req_settings,
     )
 
-    print('=' * 16 + '\n' + 'MODEL OUTPUT')
+    print('=' * 16 + '\n' + 'LLM OUTPUT')
     print(str(result))
 
 
