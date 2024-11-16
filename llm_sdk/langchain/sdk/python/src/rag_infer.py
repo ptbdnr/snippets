176fde3a0bc2a2ff6e593e3a1d5b80a5b2552288
Peter Bodnar <ptbdnr@google.com>
2024-11-16 19:01:14 +0000

    feat: optional RunnableLambda(retriever)

--- llm_sdk/langchain/sdk/python/src/rag_infer.py
+++ llm_sdk/langchain/sdk/python/src/rag_infer.py
@@ -1,9 +1,19 @@
+## Outline
+# 1: Create the LLM instance
+# 2: Connect to the vector store
+# 3: Create the prompt template / message list
+# 4: Retrieve information and generate response
+# 5: Return the result to the client
+
 import os
 import dotenv
 
 from langchain import hub
 from langchain_chroma import Chroma
-from langchain_core.runnables import RunnablePassthrough
+from langchain_core.runnables import (
+    RunnableLambda,
+    RunnablePassthrough
+)
 from langchain_openai import AzureChatOpenAI
 
 from mockup_embedding import MockupEmbedding
@@ -15,17 +25,21 @@ CHROMA_PESIST_DIR_PATH = "./chroma_cache"
 CHROMA_COLLECTION_NAME = "<ADD_COLLECTION_NAME>"
 
 
+# Create the LLM instance
 llm = AzureChatOpenAI(
     azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
     azure_deployment=os.environ["AZURE_OPENAI_DEPLOYMENT_NAME"],
     openai_api_version=os.environ["AZURE_OPENAI_API_VERSION"],
 )
 
+# Create the Embedding client
+embedding = MockupEmbedding()
 
+# Create Vector store client
 vectorstore = Chroma(
     persist_directory=CHROMA_PESIST_DIR_PATH,
     collection_name=CHROMA_COLLECTION_NAME,
-    embedding_function=MockupEmbedding()
+    embedding_function=embedding
 )
 
 # Query and retrieve data
@@ -46,6 +60,7 @@ print(retriever.invoke("What is farthest space object from here?"))
 
 # Generate response
 rag_chain = (
+    # {"context": RunnableLambda(retriever) | format_docs, "question": RunnablePassthrough()}
     {"context": retriever | format_docs, "question": RunnablePassthrough()}
     | prompt_template
     | llm
