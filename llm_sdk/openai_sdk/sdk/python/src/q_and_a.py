eb492f070fca7dd678de58c90d6137862a6c3291
Peter Bodnar <ptbdnr@google.com>
2024-11-17 17:47:19 +0000

    feat: response components

--- llm_sdk/openai_sdk/sdk/python/src/q_and_a.py
+++ llm_sdk/openai_sdk/sdk/python/src/q_and_a.py
@@ -1,4 +1,5 @@
 import os
+import json
 import dotenv
 
 from openai import AzureOpenAI
@@ -26,5 +27,22 @@ result = llm.chat.completions.create(
 print('=' * 16 + '\n' + 'LLM OUTPUT')
 print(result.choices[0].message.content)
 
-# get input token count
-print(f"input tokens: {result.usage.prompt_tokens}")
+# Parse the LLM output
+print(f"id {result.id}")
+print(f"created {result.created}")
+print(f"model {result.model}")
+print(f"choices {result.choices}")
+
+for choice in result.choices:
+    print(json.dumps({
+        "index": choice.index,
+        "finish_reason": choice.finish_reason,
+        "message.role": choice.message.role,
+        "message.content": choice.message.content,
+        "message.tool_calls": choice.message.tool_calls
+    }, indent=2))
+
+print(f"usage {result.usage}")
+print(f"prompt_tokens {result.usage.prompt_tokens}")
+print(f"completion_tokens {result.usage.completion_tokens}")
+print(f"total_tokens {result.usage.total_tokens}")
