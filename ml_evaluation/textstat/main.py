b1b29bcae0bf86f88499e63fd71da42c2474eab0
Peter Bodnar <ptbdnr@google.com>
2024-11-18 09:23:26 +0000

    feat: textstat

---
+++ ml_evaluation/textstat/main.py
@@ -0,0 +1,16 @@
+import textstat
+
+test_data = (
+    "Playing games has always been thought to be important to "
+    "the development of well-balanced and creative children; "
+    "however, what part, if any, they should play in the lives "
+    "of adults has never been researched that deeply. I believe "
+    "that playing games is every bit as important for adults "
+    "as for children. Not only is taking time out to play games "
+    "with our children and other adults valuable to building "
+    "interpersonal relationships but is also a wonderful way "
+    "to release built up tension."
+)
+
+print(f"FK grade: {textstat.flesch_kincaid_grade(test_data)}")
+print(f"ARI: {textstat.automated_readability_index(test_data)}")
