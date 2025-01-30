3b0877c68e1e310955231a08c5f2e3672d1c7d77
Peter Bodnar <ptbdnr@google.com>
2025-01-30 00:59:33 +0000

    feat: named aggregation

--- datahandling/pandas/main.py
+++ datahandling/pandas/main.py
@@ -9,11 +9,6 @@ Id;Value A;Value B;Value C;Quantity;Valid
 3;14;8;8;12;NO
 4;15;9;9;23;NO
 5;16;7;5;46;NO
-6;17;7;7;6;YES
-7;18;8;8;4;NO
-8;19;9;9;8;NO
-9;20;10;15;5;NO
-10;21;11;11;90;YES
 """)
 
 # Get the IDs for the rows that have the sum of Value B and Value C greater than Value A and have the column Valid set to YES
@@ -21,9 +16,36 @@ Id;Value A;Value B;Value C;Quantity;Valid
 
 df = pd.read_csv(data, delimiter=";")
 df.info()
+df.describe()
+df.head()
 
-df['result']=df.apply(lambda row: 1 if (row['Value B']+row['Value C']>row['Value A']) and (row['Valid']=='YES') else 0 , axis=1 )
 
+# Data Filtering
+df['result']=df.apply(
+    lambda row: 1 if (row['Value B']+row['Value C']>row['Value A']) and (row['Valid']=='YES') else 0,
+    axis=1,
+)
 list(df[df.result==1].Id)
 
-df[df.result==1].Quantity.sum()
+
+# Grouping Data
+
+## Example 1: Basic aggregation
+grouped_basic = df.groupby('Valid').sum()
+print("Basic Aggregation:\n", grouped_basic)
+
+## Example 2: Custom aggregation function
+grouped_custom = df.groupby('Valid').agg({'Quantity': 'sum', 'Value A': 'mean'})
+print("Custom Aggregation Function:\n", grouped_custom)
+
+def my_custom_function(x):
+    return x.max() - x.min()
+
+grouped_custom_function = df.groupby('Valid').agg({'Quantity': my_custom_function, 'Value A': 'mean'})
+
+## Example 3: Named aggregation
+grouped_named = df.groupby('Valid').agg(
+    total_quantity=pd.NamedAgg(column='Quantity', aggfunc='sum'),
+    average_value_a=pd.NamedAgg(column='Value A', aggfunc='mean')
+)
+print("Named Aggregation:\n", grouped_named)
