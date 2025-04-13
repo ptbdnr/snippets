f799b7d59ed05d1c5fc60acb9f5572afc7bccd12
Peter <ptbdnr@gmail.com>
2025-04-13 13:02:48 +0100

    nextjs js

---
+++ webapp/nextjs_js/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#FF7043",
+        },
+        height: {
+          "15": "60px",
+        },
+      },
+    },
+  };
